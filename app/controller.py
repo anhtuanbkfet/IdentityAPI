da6f7c9fdf4ec35cc987520d48e54f0659e6018d
Anh Tuấn <anhtuan.bkfet.k55@gmail.com>
2023-11-17 10:28:55 +0700

    change api code

--- app/controller.py
+++ app/controller.py
@@ -1,8 +1,28 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+"""
+Check the Id of an identity is valid or not (Valid if this Id is existed on FIS database)
+"""
+@router.get("/check-valid")
+async def check_valid(id=0):
+    ret = True if int(id) % 2 == 0 else False 
 
-@router.get("/check_exist")
-async def check_exist(id=0):
-    result = {"result": "OK"}
-    return result
+    identity ={
+            "id": id,
+            "username": "admin",
+            "roles": ["admin"],
+            "token": None,
+        }
+    if ret:
+        return {
+        "success": ret,
+        "message": "Identity is valid and can be used",
+        "result": identity,
+        }
+    else:
+        return {
+            "success": ret,
+            "code": -1,
+            "message": "Identity is not found",
+        }
