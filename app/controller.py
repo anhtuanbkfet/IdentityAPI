8452c1c0a9c93d41fe309e9d6829934b0604ba28
AnhTuan <anhtuan.bkfet.k55@gmail.com>
2024-01-11 04:22:39 +0000

    Add some functions to api

--- app/controller.py
+++ app/controller.py
@@ -1,6 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile, Request
+from typing import Any, List
+from typing_extensions import Annotated
+import os
+
 
 router = APIRouter()
+
+UPLOADED_FOLDER = "uploaded_files"
+
+
+def save_file(saved_dir, file_data, filename):
+    if not os.path.isdir(saved_dir):
+        os.mkdir(saved_dir)
+    file_path = os.path.join(saved_dir, filename.replace(" ", "-"))
+    with open(file_path,'wb+') as f:
+        f.write(file_data)
+        f.close()
+    return filename
+
+
 """
 Check the Id of an identity is valid or not (Valid if this Id is existed on FIS database)
 """
@@ -26,3 +44,49 @@ async def check_valid(id=0):
             "code": -1,
             "message": "Identity is not found",
         }
+
+from time import time
+@router.post("/file/")
+async def create_file(file: Annotated[bytes, File()]):
+    saved_dir = os.path.join(os.getcwd(), UPLOADED_FOLDER)
+    filename = "{}".format(time())
+    file_name = save_file(saved_dir, file, filename)
+    return {
+        "success": True,
+        "file": file_name,
+        "file_size": len(file),
+        }
+
+
+@router.post("/uploadfile/")
+async def create_upload_file(image: UploadFile=File(...)):
+    """
+Uppload single image file into server.
+    """
+    saved_dir = os.path.join(os.getcwd(), UPLOADED_FOLDER)
+
+    file_name = save_file(saved_dir, image.file.read(), image.filename)
+
+    return {
+        "success": True,
+        "file": file_name,
+        "file_size": len(image.file.read())
+        }
+
+
+@router.post("/uploadfiles/")
+async def create_upload_files(images: List[UploadFile]=File(...)):
+    """
+Uppload image files into server.
+    """
+    saved_dir = os.path.join(os.getcwd(), UPLOADED_FOLDER)
+    saved_files = []
+    
+    for image in images:
+        saved_files.append(save_file(saved_dir, image.file.read(), image.filename))
+    return {
+        "success": True,
+        "files": saved_files,
+        }
+
+
